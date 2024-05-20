02394ad56ae7bfd0347bff7bd528a6a10fb4b448
Pablo Fortich <152929248+fortichpablo@users.noreply.github.com>
2024-05-20 18:56:20 -0400

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -6,11 +6,10 @@ import dash_bootstrap_components as dbc
 home_content = '''
 ## What is churn?
 This is where we define/describe churn.
-
-
+Churn, also known as customer attrition, refers to the phenomenon where customers stop doing business with a company or cease using its services over a specific period or permanently. 
 ## What does churn mean for my business?
 This is where we tell them why churn is important for them to consider.
-
+It is a critical metric in industries like telecommunications, subscription-based services, and financial services because retaining existing customers is often more cost-effective than acquiring new ones.
 
 ## What can I do to stop churn?
 This is where we tell them how we can help.
