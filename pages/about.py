d884652e429059a6164d941eec016b58ed013714
Pablo Fortich <152929248+fortichpablo@users.noreply.github.com>
2024-05-20 20:00:51 -0400

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -34,7 +34,9 @@ tab3_content = dbc.Card(
 
 tab4_content = dbc.Card(
     dbc.CardBody([
-        html.P("Pablo Fortich. Graduate from Universitty of Central Florida class of 2024. Soon to attend Georgia Tech to earn my masters in Analytics.", className="card=text"),
+        html.P("Hello! I'm Pablo Fortich, a recent graduate with a Master’s degree in Political Science, specializing in Intelligence and National Security, from the University of Central Florida. I am also currently enrolled in the Master's in Analytics program at Georgia Tech, where I am further honing my skills in data analytics to support my aspiration of working in a data-driven role within the federal government.
+
+", className="card=text"),
         dbc.Button("Click here", color="success"),
     ]),
     className="mt-3",
