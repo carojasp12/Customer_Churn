6129cf5eddeb40728d4816db6829d460dad1f5ef
Meagan Coldewe <yamoriami@yahoo.com>
2024-05-25 16:23:26 -0400

    updated about and home pages

--- pages/about.py
+++ pages/about.py
@@ -3,40 +3,68 @@ from dash import html, dcc
 import dash_bootstrap_components as dbc
 
 
+about_harsh = """
+Harsh brings a background in project management and microbiology to his work in data analytics. 
+His commitment to exhaustive and detailed analysis ensures a robust data picture. 
+He's proficient with Python and machine learning, and aspires to expand his knowledge and effectiveness within the field.
+"""
+
+about_cesar = """
+Cesar brings an array of experiences, from roofing to hospitality to travel photography, to his love of mathematics and data analysis. 
+Persistent and driven, Cesar leverages his studies in statistics to ensure complete and accurate results from any code he writes. 
+He aspires to bring new technologies to fields that have largely been analog.
+"""
+
+about_meagan = """
+Meagan combines her skills in HTML, Plotly Dash, Python, and data visualization with a steadfast persistence and eagerness to learn. 
+With a background in English, she brings a unique perspective to data analysis, excelling in project management and effective communication. 
+Megan is driven to transition into the data analysis field, where she aims to leverage her strong analytical capabilities and creative problem-solving skills to deliver insightful and impactful results. 
+"""
+
+about_pablo = """
+Pablo leverages his drive and passion for sales to make decisions quickly with an eye for best customer result. 
+He brings a background in political science, with specialties in Intelligence and National Security, to write code sensitive to the current data market, and seeks to enhance his knowledge and understanding of the field through his current enrollment in the Masters in Analytics program at Georgia Tech. 
+He aspires to combine his passions in a data-driven role within the federal government.
+"""
+
 tab1_content = dbc.Card(
     dbc.CardBody(
         [
-            dcc.Markdown("This is tab 1!", className="card-text"),
+            html.P(about_cesar, className="card-text"),
             dbc.Button("Find Cesar on Github", color="secondary", href="https://github.com/carojasp12"),
+            dbc.Button("Find Cesar on LinkedIn", color="success"),
         ]
     ),
-    className="mt-3",
+    className="mt-3 fs-5 px-0",
 )
 
 tab2_content = dbc.Card(
     dbc.CardBody(
         [
-            html.P("Hello! My name is Harsh. I am passionate to embark on a career in data science with a background in project management. My commitment to detail-oriented analysis and a recent professional certificate in data science, including proficiency in Python and machine learning, underscores my readiness to contribute effectively to data projects.", className="card-text"),
+            html.P(about_harsh, className="card-text"),
             dbc.Button("Find Harsh on Github", color="secondary", href="https://github.com/10H-K"),
+            dbc.Button("Find Harsh on LinkedIn", color="success"),
         ]
     ),
-    className="mt-3",
+    className="mt-3 fs-5 px-0",
 )
 
 tab3_content = dbc.Card(
     dbc.CardBody([
-        html.P("This is card 3. Don't shoot", className="card=text"),
+        html.P(about_meagan, className="card=text"),
         dbc.Button("Find Meagan on Github", color="secondary", href="https://github.com/m-coldewe"),
+        dbc.Button("Find Meagan on LinkedIn", color="success"),
     ]),
-    className="mt-3",
+    className="mt-3 fs-5 px-0",
 )
 
 tab4_content = dbc.Card(
     dbc.CardBody([
-        html.H6("Hello! I'm Pablo Fortich, a recent graduate with a Bachelors degree in Political Science, specializing in Intelligence and National Security, from the University of Central Florida. I am also currently enrolled in the Master's in Analytics program at Georgia Tech, where I am further honing my skills in data analytics to support my aspiration of working in a data-driven role within the federal government.", className="card=text"),
-        dbc.Button("Click here", color="success"),
+        html.P(about_pablo, className="card=text"),
+        dbc.Button("Find Pablo on Github", color="secondary", href="https://github.com/fortichpablo"),
+        dbc.Button("Find Pablo on LinkedIn", color="success"),
     ]),
-    className="mt-3",
+    className="mt-3 fs-5 px-0",
 )
 
 tabs = dbc.Tabs(
@@ -48,24 +76,9 @@ tabs = dbc.Tabs(
     ]
 )
 
-# layout = dbc.Container([
-#     dbc.Row([]),
-#     dbc.Row([
-#         dbc.Col([
-#             html.H1("About Perfections-Under-Pressure+")
-#         ])
-#     ]),
-#     dbc.Row([]),
-#     dbc.Row([
-#         dbc.Col([
-#             tabs
-#         ])
-#     ]),
-#     dbc.Row
-# ])
 
 layout = dbc.Container([
-    html.Br(), html.H1("About Perfections-Under-Pressure+"),
+    html.Br(), html.H1("About Perfectionists-Under-Pressure+"),
     tabs,
 ])
 
--- pages/home.py
+++ pages/home.py
@@ -4,42 +4,80 @@ import dash_bootstrap_components as dbc
 
 image_path = 'assets/churn-image.jpg'
 
-home_content = '''
-## So what is churn?
-This is where we define/describe churn.
-Churn, also known as customer attrition, refers to the phenomenon where customers stop doing business with a company or cease using its services over a specific period or permanently. 
-## What does churn mean for my business?
-This is where we tell them why churn is important for them to consider.
-It is a critical metric in industries like telecommunications, subscription-based services, and financial services because retaining existing customers is often more cost-effective than acquiring new ones.
+churn_header = "So what is churn?"
 
-## These are the top 3 reasons why customers churn or not churn?
+churn_body = """
+'Churn,' also know as customer attrition, describes the phenomenon where a customer ends their relationship with a company, frequently for a competing service.
+The separation can be permanent or temporary, but it results in a complete termination of services for the duration.
+"""
 
-The top three reasons according to our model that will likely cause a customer to churn are as follows
+churn_business_header = "What does churn mean for my business?"
 
-1. Higher call rate to the support department
-2. Customer is paying past the payment due date on a consistent basis
-3. The customer has a high total spend overtime 
+churn_business_body1 = """
+Since churn describes the erosion of a business's customer base, it results in lost revenue.
+When considering a single customer, that might not seem like much.
+But what if you were looking at your entire customer base?
+"""
 
-The top 3 reasons that do not affect if a customer is going or churn or not churn
+churn_business_body2 = """
+The metric generally used to look at churn is churn rate, which calculates the number of customers dropping a business's services within a given timeframe.
+Churn rate is a critical metric, especially within the telecommunication and financial inductries, which rely on subscription services to maintain their revenue.
+"""
 
-1. Gender
-2. The type of subcription (Basic, Standard, Premium)
-3. Usage frequency
+churn_business_body3 = """
+The purpose of this project isn't to identify how quickly you'ore losing customers, though.
+It's to help you identify who you're losing so you can employ better-targeted marketing campaigns aimed at retaining your customer base.
+Or identify prblems within your business that are pushing your customers to leave.
+"""
 
-Hypothesis - We infer that a customer will not churn 
+help_header = "How can you help me determine which customers are leaving?"
 
-1. The product is excellent meaning that there will be less support calls
-2. The customer pays on a consistent basis meaning they enjoy and use the product
-3. The customer has a low spend overtime
+help_body = """
+Through machine learning and data analysis. 
+You can learn more, and even take advantage of our model, by checking out our Data, Model, and Dashboard pages, visible from the top of every page.
+Or you can follow the links!
+"""
 
+help_body2 = "To learn more about the members of Perfectionists-Under-Pressure+, check out our About page."
 
-'''
+more_header = "But I want to know more now!"
+
+more_body1 = "Sure!"
+
+more_body2 = "According to our data, the most important features related to churn are:"
+
+more_body3 = "1. Support calls"
+
+more_body4 = "2. Delayed payment, and"
+
+more_body5 = "3. Total spend"
+
+more_body6 = """
+Very likely, without any additional information, those details are painting a picture for you.
+Perhaps you, like me, imagine that a high number of support calls, and excessive or consistent tardiness marking payments, makes a customer likely to churn.
+Maybe we imagine that a customer who has invested a lot of money in the company is more likely to stay.
+"""
+
+more_body7 = "But what if it's not that simple?"
+
+more_body8 = "Where before we looked at the most important features, let's look at the least:"
+
+more_body9 = "3. Usage frequency"
+
+more_body10 = "2. Type of subscription (Basic, Standard, Premium)"
+
+more_body11 = "1. Gender"
+
+more_body12 = "But does 'less important' mean 'not important at all1?"
+
+more_body13 = "Upload some customer data to our model and let it tell you, or decide for yourself."
+
 
 layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             html.H1("To Churn or Not to Churn")
-        ])
+        ], className="mt-4")
     ]),
     dbc.Row([
         dbc.Col([
@@ -48,13 +86,72 @@ layout = dbc.Container([
     ]),
     dbc.Row([
         dbc.Col([
-            html.H3("A Machine Learning Project by: Perfectionists-Under_pressure+")
+            html.H1("A Machine Learning Project by: Perfectionists-Under-pressure+")
         ])
     ]),
     dbc.Row([
         dbc.Col([
-            dcc.Markdown(home_content)
-        ])
+            html.H2(churn_header)
+        ], className="mt-5")
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.P(churn_body)
+        ], className="fs-5")
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.H2(churn_business_header)
+        ], className="mt-3")
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.P(churn_business_body1),
+            html.Br(),
+            html.P(churn_business_body2),
+            html.Br(),
+            html.P(churn_business_body3)
+        ], className="fs-5")
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.H2(help_header)
+        ], className="mt-3")
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.P(help_body),
+            html.Br(),
+            html.P(help_body2)
+        ], className="fs-5")
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.H2(more_header)
+        ], className="mt-3")
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.P(more_body1),
+            html.Br(),
+            html.P(more_body2),
+            html.Ol(more_body3),
+            html.Ol(more_body4),
+            html.Ol(more_body5),
+            html.Br(),
+            html.P(more_body6),
+            html.Br(),
+            html.P(more_body7),
+            html.Br(),
+            html.P(more_body8),
+            html.Ol(more_body9),
+            html.Ol(more_body10),
+            html.Ol(more_body11),
+            html.Br(),
+            html.P(more_body12),
+            html.Br(),
+            html.P(more_body13)
+        ], className="fs-5")
     ])
 ])
 
